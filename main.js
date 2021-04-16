79f54ccf5c53f51a86f1c8e005f45f11e9af529f
Herom123 <63053396+Herom123@users.noreply.github.com>
2021-04-16 17:47:18 +0300

    ei

---
+++ main.js
@@ -0,0 +1,142 @@
+particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 355,
+        "density": {
+          "enable": true,
+          "value_area": 789.1476416322727
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.48927153781200905,
+        "random": false,
+        "anim": {
+          "enable": true,
+          "speed": 0.2,
+          "opacity_min": 0,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 2,
+        "random": true,
+        "anim": {
+          "enable": true,
+          "speed": 2,
+          "size_min": 0,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": false,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 0.2,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "bubble"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 83.91608391608392,
+          "size": 1,
+          "duration": 3,
+          "opacity": 1,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  });
+
+  var textHolder  = document.querySelector(".container .content span");
+  var container   = document.getElementsByClassName("container")[0];
+  var content     = document.querySelector(".container .content");
+  var textInUrl   = decodeURI(window.location.hash.slice(1));
+  var ClickMeSpan = document.querySelector('#ClickMe span');
+  var audio       = document.getElementById('typingSound');
+  var ClickMe     = document.getElementById('ClickMe');
+
+  function c(toggle=true){
+    if(toggle) container.style.display="block"
+    else container.style.display="none"
+  }
+
+  c(false)
+  audio.load();
+  textHolder.innerHTML = textInUrl?textInUrl:"Red Was Not Imposter";
+
+  var a = setInterval(()=>{
+
+	let rightY = content.offsetLeft+content.offsetWidth;
+    let minwidth = rightY>textHolder.offsetLeft                         - 100;
+    let maxwidth = rightY<textHolder.offsetLeft+textHolder.offsetWidth  - 100;
+
+	if(minwidth&&maxwidth){
+        audio.play();
+    } else {
+        audio.pause();
+    }
+},200);
+
+setTimeout(()=>clearInterval(a),10000);
